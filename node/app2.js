bb873183172cb6e7c181c0849938ff33eb60cdf9
hazzzi <hanee_l@naver.com>
2021-01-09 18:12:14 +0900

    update app2, circle

--- node/app2.js
+++ node/app2.js
@@ -1,4 +1,8 @@
 const circle = require('./circle.js');
+const myCircle = circle(4)
 
-console.log(`지름이 4인 원의 면적: ${circle.area(4)}`)
-console.log(`지름이 4인 원의 둘레: ${circle.circumference(4)}`)
+// console.log(`지름이 4인 원의 면적: ${circle.area(4)}`)
+// console.log(`지름이 4인 원의 둘레: ${circle.circumference(4)}`)
+
+console.log(`지름이 4인 원의 면적: ${myCircle.area(4)}`)
+console.log(`지름이 4인 원의 둘레: ${myCircle.circumference(4)}`)
--- node/circle.js
+++ node/circle.js
@@ -3,5 +3,12 @@ const { PI } = Math;
 
 // exports 객체의 메소드로 정의
 // 외부에 공개됨
-exports.area = (r) => PI * r * r;
-exports.circumference = (r) => 2 * PI * r;
+// exports.area = (r) => PI * r * r;
+// exports.circumference = (r) => 2 * PI * r;
+
+module.exports = function (r) {
+    return {
+        area() { return PI * r * r; },
+        circumference() { return 2 * PI * 2; }
+    };
+}
