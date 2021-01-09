12247a01122bad6fa5ce8e783bdc8c67f27c389a
hazzzi <hanee_l@naver.com>
2021-01-09 18:21:44 +0900

    update app2

--- node/app2.js
+++ node/app2.js
@@ -4,5 +4,13 @@ const myCircle = circle(4)
 // console.log(`지름이 4인 원의 면적: ${circle.area(4)}`)
 // console.log(`지름이 4인 원의 둘레: ${circle.circumference(4)}`)
 
-console.log(`지름이 4인 원의 면적: ${myCircle.area(4)}`)
-console.log(`지름이 4인 원의 둘레: ${myCircle.circumference(4)}`)
+// console.log(`지름이 4인 원의 면적: ${myCircle.area(4)}`)
+// console.log(`지름이 4인 원의 둘레: ${myCircle.circumference(4)}`)
+
+const myModule = require('./module');
+
+const result = myModule.calc.add(1, 2);
+
+console.log(result)
+
+myModule.print.sayHello()
