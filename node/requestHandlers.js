61ec893e912a76b95a78984e2de8380effc2ed87
hazzzi <hanee_l@naver.com>
2021-01-11 18:15:21 +0900

    update request handler

--- node/requestHandlers.js
+++ node/requestHandlers.js
@@ -1,19 +1,22 @@
 const exec = require("child_process").exec;
 
-const start = () => {
+const start = (response) => {
     console.log("Request handler 'start' was called.");
-    let content = "empty";
 
     exec("ls -lah", function (error, stdout, stderr) {
-        content = stdout;
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write(stdout);
+        response.end();
     })
 
     return content;
 }
 
-const upload = () => {
+const upload = (response) => {
     console.log("Requeset handler 'upload' was called.");
-    return "Hello Upload"
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Hello Upload");
+    response.end();
 }
 
 exports.start = start;
--- node/server.js
+++ node/server.js
@@ -19,10 +19,10 @@ const start = (route, handle) => {
         var pathname = url.parse(request.url).pathname;
         console.log(`Request for ${pathname} received.`)
 
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        const content = route(handle, pathname);
-        response.write(content);
-        response.end();
+        const content = route(handle, pathname, response);
+        // response.writeHead(200, { "Content-Type": "text/plain" });
+        // response.write(content);
+        // response.end();
     }
 
     http.createServer(onRequest).listen(8888)
