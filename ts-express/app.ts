b2dea51c4c2e012c1dbde9e19103d5cd08312e51
hazzzi <hanee_l@naver.com>
2021-01-11 19:30:37 +0900

    add note

--- ts-express/app.ts
+++ ts-express/app.ts
@@ -1,7 +1,15 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', (req: express.Request, res: express.Response) => res.send('Hello world'))
 
+app.post('/signin', (req: express.Request, res: express.Response) => {
+    const { username, password } = req.body;
+    res.send({ username, password });
+})
 app.listen(3000, () => console.log('Example app listening on port 3000'))
---
+++ ts-express/note.md
@@ -0,0 +1,6 @@
+# express
+
+## 1
+- routing: 클라이언트 요청에 응답하는 방법을 결정
+  - 각 라우터는 하나이상의 핸들러 함수를 가질수있다
+  - app.method(path, handler)
